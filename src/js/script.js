df1420b8381886b7e187a554825b1851a011539e
sema ilchuk <semailchuk2000@gmail.com>
2021-11-05 23:53:05 +0700

    hamburger menu made

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeMenu = document.querySelector('.menu__close'),
+      menuLinks = document.querySelectorAll('.menu__list li');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+for (let menuLink of menuLinks )
+    menuLink.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
